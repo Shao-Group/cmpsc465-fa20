962954ce9fa8dbe02aed610868b6b561f32a27d6
Justin Huang <xinglongjh@gmail.com>
2020-09-05 13:32:07 -0400

    Fixed integer comparison in problem 1

--- assignment1/problem1/solution.py
+++ assignment1/problem1/solution.py
@@ -12,8 +12,9 @@ elif n == '0':
     print(list_1)
 else:
     a = list_1[2:].split()
+    print(a)
     b = list_2[2:].split()
-
+    print(b)
     c = [str(int(m) + int(n))]
 
     while a or b:
@@ -22,7 +23,7 @@ else:
         elif not a:
             c.append(b.pop(0))
         else:
-            if a[0] < b[0]:
+            if int(a[0]) < int(b[0]):
                 c.append(a.pop(0))
             else:
                 c.append(b.pop(0))
